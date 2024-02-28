f2a8d6523b8b21fab2b3bb7c33a375a22188a138
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-02-28 08:38:42 +0100

    IntersectionResult2D Implemented for ISegmentable2D

--- DiGi.Geometry/Planar/Create/IntersectionResult2D.cs
+++ DiGi.Geometry/Planar/Create/IntersectionResult2D.cs
@@ -2,6 +2,7 @@
 using DiGi.Geometry.Planar.Classes;
 using DiGi.Geometry.Planar.Interfaces;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DiGi.Geometry.Planar
 {
@@ -142,6 +143,78 @@ namespace DiGi.Geometry.Planar
             }
 
             return new IntersectionResult2D(geometry2Ds);
+        }
+
+        public  static IntersectionResult2D IntersectionResult2D(this IEnumerable<Segment2D> segment2Ds_1, IEnumerable<Segment2D> segment2Ds_2, double tolerance = Constans.Tolerance.Distance)
+        {
+            if(segment2Ds_1 == null || segment2Ds_2 == null)
+            {
+                return null;
+            }
+
+            if (segment2Ds_1.Count() == 0 || segment2Ds_2.Count() == 0)
+            {
+                return new IntersectionResult2D();
+            }
+
+            List<Point2D> point2Ds_Intersection = new List<Point2D>();
+            List<Segment2D> segment2Ds_Intersection = new List<Segment2D>();
+            foreach (Segment2D segment2D_Temp in segment2Ds_1)
+            {
+                if (segment2D_Temp == null)
+                {
+                    continue;
+                }
+
+                IntersectionResult2D intersectionResult2D = IntersectionResult2D(segment2D_Temp, segment2Ds_2);
+                if (intersectionResult2D == null || !intersectionResult2D.Intersect)
+                {
+                    continue;
+                }
+
+                IGeometry2D geometry2D = intersectionResult2D.GetGeometry2Ds<IGeometry2D>()[0];
+                if (geometry2D is Point2D)
+                {
+                    Point2D point2D_Intersection = (Point2D)geometry2D;
+                    Point2D point2D_Similar = point2Ds_Intersection.Find(x => x.Similar(point2D_Intersection, tolerance));
+                    if (point2D_Similar == null)
+                    {
+                        point2Ds_Intersection.Add(point2D_Intersection);
+                    }
+                }
+                else if (geometry2D is Segment2D)
+                {
+                    Segment2D segment2D_Intersection = (Segment2D)geometry2D;
+                    Segment2D segment2D_Similar = segment2Ds_Intersection.Find(x => x.Similar(segment2D_Intersection, tolerance));
+                    if (segment2D_Similar == null)
+                    {
+                        segment2Ds_Intersection.Add(segment2D_Intersection);
+                    }
+                }
+            }
+
+            List<IGeometry2D> geometry2Ds = new List<IGeometry2D>();
+            for (int i = 0; i < point2Ds_Intersection.Count; i++)
+            {
+                geometry2Ds.Add(point2Ds_Intersection[i]);
+            }
+
+            for (int i = 0; i < segment2Ds_Intersection.Count; i++)
+            {
+                geometry2Ds.Add(segment2Ds_Intersection[i]);
+            }
+
+            return new IntersectionResult2D(geometry2Ds);
+        }
+
+        public static IntersectionResult2D IntersectionResult2D(this ISegmentable2D segmentable2D_1, ISegmentable2D segmentable2D_2, double tolerance = Constans.Tolerance.Distance)
+        {
+            if(segmentable2D_1 == null || segmentable2D_2 == null)
+            {
+                return null;
+            }
+
+            return IntersectionResult2D(segmentable2D_1.GetSegments(), segmentable2D_2.GetSegments(), tolerance);
         }
     }
 
