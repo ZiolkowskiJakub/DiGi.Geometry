b31121dea0372de2dceea4dacb129f618d2b83c8
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-06-07 12:30:17 +0200

    Centroid calculation updated

--- DiGi.Geometry/Spatial/Query/Centroid.cs
+++ DiGi.Geometry/Spatial/Query/Centroid.cs
@@ -8,18 +8,46 @@ namespace DiGi.Geometry.Spatial
     {
         public static Point3D Centroid(this IEnumerable<Point3D> point3Ds)
         {
-            if (point3Ds == null || point3Ds.Count() < 3)
+            if (point3Ds == null)
             {
                 return null;
+            }
+
+            int count = point3Ds.Count();
+            if(count == 0)
+            {
+                return null;
+            }
+
+            Point3D point3D_1 = point3Ds.ElementAt(0);
+
+            if (count == 1)
+            {
+                return new Point3D(point3D_1);
+            }
+
+            Point3D point3D_2 = point3Ds.ElementAt(1);
+
+            if (count == 2)
+            {
+                return new Segment3D(point3D_1, point3D_2).Mid();
+            }
+
+            if (count == 3)
+            {
+                Point3D point3D_3 = point3Ds.ElementAt(1);
+
+                double centroidX = (point3D_1.X + point3D_2.X + point3D_3.X) / 3.0;
+                double centroidY = (point3D_1.Y + point3D_2.Y + point3D_3.Y) / 3.0;
+                double centroidZ = (point3D_1.Z + point3D_2.Z + point3D_3.Z) / 3.0;
+
+                return new Point3D(centroidX, centroidY, centroidZ);
             }
 
             Vector3D vector3D = Constans.Vector3D.Zero;
             double area = 0;
 
-            Point3D point3D_1 = point3Ds.ElementAt(0);
-            Point3D point3D_2 = point3Ds.ElementAt(1);
-
-            for (var i = 2; i < point3Ds.Count(); i++)
+            for (var i = 2; i < count; i++)
             {
                 Point3D point3D_3 = point3Ds.ElementAt(i);
                 Vector3D vector3D_1 = new Vector3D(point3D_1, point3D_3);
