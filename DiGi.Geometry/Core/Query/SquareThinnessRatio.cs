3df2cf387937e4cd83c18c2b0ff8beead5f912a3
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-05-20 07:17:19 +0200

    SquareThinnessRatio summary updated

--- DiGi.Geometry/Core/Query/SquareThinnessRatio.cs
+++ DiGi.Geometry/Core/Query/SquareThinnessRatio.cs
@@ -3,11 +3,11 @@
     public static partial class Query
     {
         /// <summary>
-        /// The Rectangular Thinness Ratio (RTR) — also sometimes called the Rectangular Compactness Ratio or Rectangular Shape Factor — is a geometric shape descriptor that measures how elongated or “thin” a shape is compared to a rectangle that perfectly fits around it (its minimum bounding rectangle).
+        /// The Square Thinness Ratio (STR) — also sometimes called the Square Compactness Ratio or Square Shape Factor — is a geometric shape descriptor that measures how elongated or “thin” a shape is compared to a square that perfectly fits around it (its minimum bounding square).
         /// </summary>
         /// <param name="area">shape area</param>
         /// <param name="squareArea">square area</param>
-        /// <returns>Rectangular thinness ratio</returns>
+        /// <returns>Square thinness ratio</returns>
         public static double SquareThinnessRatio(double area, double squareArea)
         {
             if (double.IsNaN(area) || double.IsNaN(squareArea))
